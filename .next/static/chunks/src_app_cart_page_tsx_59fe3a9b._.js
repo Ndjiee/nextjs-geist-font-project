de53c18137f319ad77c8a9f5502279ce97790c46
Ndjiee <frdlqueen8@gmail.com>
2025-06-17 20:24:07 -0700

    Add .next/static/chunks/src_app_cart_page_tsx_59fe3a9b._.js 1750217046899

---
+++ .next/static/chunks/src_app_cart_page_tsx_59fe3a9b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d03248bd._.js",
+  "static/chunks/src_7bac396a._.js"
+],
+    source: "dynamic"
+});
