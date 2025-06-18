c9a01032e51184cd90e597edbe839b6d3e104fd5
Ndjiee <frdlqueen8@gmail.com>
2025-06-17 20:24:17 -0700

    Add .next/static/chunks/src_app_page_tsx_59fe3a9b._.js 1750217057258

---
+++ .next/static/chunks/src_app_page_tsx_59fe3a9b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7b16c1dd._.js",
+  "static/chunks/src_3e97c7ce._.js"
+],
+    source: "dynamic"
+});
